6bf20dcadcc719e005ce4686a06e1e14d7cad6c3
Gustavo Passos <gustavofsp@hotmail.com>
2020-09-08 11:38:10 +0100

    Jogada Especial Promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -196,6 +196,18 @@ namespace xadrez {
                 throw new TabuleiroException("Voce nao pode se colocar em Xeque!");
 
             }
+            Peca p = Tab.Peca(destino);
+            //#Jogada especial Promocao
+            if (p is Peao) {
+                if ((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7)) {
+                    p = Tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+
+            }
             if (EstaEmXeque(Adversaria(JogadorAtual))) {
                 Xeque = true;
             }
@@ -209,7 +221,7 @@ namespace xadrez {
                 Turno++;
                 MudarJogador();
             }
-            Peca p = Tab.Peca(destino);
+           
 
             //#Jogada Especial en passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2)) {
